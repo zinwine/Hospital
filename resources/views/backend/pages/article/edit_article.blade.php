0885d948a2b4cd815808177958c9bdb5152285da
Zin Wine <www.zinwine.mm@gmail.com>
2023-02-18 22:42:03 +0630

    delete photo and fix edit_article file

--- resources/views/backend/pages/article/edit_article.blade.php
+++ resources/views/backend/pages/article/edit_article.blade.php
@@ -13,11 +13,11 @@
             @endforeach
             <div class="form-group">
               <label for="title">Title</label>
-              <input type="text" class="form-control" name="title" id="title" placeholder={{$article->title}}>
+              <input type="text" class="form-control" name="title" id="title" value={{$article->title}}>
             </div>
             <div class="form-group">
               <label for="author">Author</label>
-              <input type="text" class="form-control" name="author" id="author" placeholder={{$article->author}}>
+              <input type="text" class="form-control" name="author" id="author" value={{$article->author}}>
             </div>
             <div class="form-group">
               <label>Upload Image</label>
